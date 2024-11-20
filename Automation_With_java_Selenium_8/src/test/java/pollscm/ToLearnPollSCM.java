ef6ecf27134ff2ad50d3d346eeda59910d6e010d
Akshitha8597 <akshitham335@gmail.com>
2024-11-20 09:28:18 +0530

    PollSCM executed

---
+++ Automation_With_java_Selenium_8/src/test/java/pollscm/ToLearnPollSCM.java
@@ -0,0 +1,13 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnPollSCM {
+
+	@Test
+	public void pollScm() {
+		Reporter.log("Poll SCM executed", true);
+
+	}
+}
